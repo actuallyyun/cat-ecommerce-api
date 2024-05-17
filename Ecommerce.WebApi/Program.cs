6c323a8363233b696d57a350c4e66989911885f2
Yun Ji <this.jiyun@gmail.com>
2024-05-17 10:56:32 +0300

    update swagger setting

--- Ecommerce.WebApi/Program.cs
+++ Ecommerce.WebApi/Program.cs
@@ -11,13 +11,28 @@ using Ecommerce.WebApi.src.Service;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(
+    options =>
+    {
+        options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+        {
+            Description = "Bearer token authentication",
+            Name = "Authorization",
+            In = ParameterLocation.Header,
+            Scheme = "Bearer"
+        }
+        );
+        options.OperationFilter<SecurityRequirementsOperationFilter>();
+    }
+);
 
 //Add all controllers
 builder.Services.AddControllers();
@@ -28,24 +43,23 @@ builder.Services.AddDbContext<EcommerceDbContext>(options =>
 });
 
 // service registration -> automatically create all instances of dependencies
-builder.Services.AddScoped<IPasswordService,PasswordService>();
+builder.Services.AddScoped<IPasswordService, PasswordService>();
 builder.Services.AddScoped<IUserRepository, UserRepo>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<ITokenService, TokenService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<ExceptionHandlerMiddleware>();
-builder.Services.AddScoped<ICategoryRepository,CategoryRepo>();
-builder.Services.AddScoped<ICategoryService,CategoryService>();
-builder.Services.AddScoped<IProductRepository,ProductRepo>();
-builder.Services.AddScoped<IProductService,ProductService>();
-builder.Services.AddScoped<IOrderRepository,OrderRepo>(); 
-builder.Services.AddScoped<IOrderService,OrderService>();
-builder.Services.AddScoped<IReviewRepository,ReviewRepo>();
-builder.Services.AddScoped<IReviewService,ReviewService>();
-builder.Services.AddScoped<IAddressRepository,AddressRepo>();
-builder.Services.AddScoped<IOrderItemRepo,OrderItemRepo>();
-builder.Services.AddScoped<IAddressService,AddressService>();
-
+builder.Services.AddScoped<ICategoryRepository, CategoryRepo>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IProductRepository, ProductRepo>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IOrderRepository, OrderRepo>();
+builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddScoped<IReviewRepository, ReviewRepo>();
+builder.Services.AddScoped<IReviewService, ReviewService>();
+builder.Services.AddScoped<IAddressRepository, AddressRepo>();
+builder.Services.AddScoped<IOrderItemRepo, OrderItemRepo>();
+builder.Services.AddScoped<IAddressService, AddressService>();
 
 // Add authentication instructions
 builder
@@ -67,25 +81,30 @@ builder
 
 builder.Services.AddAutoMapper(typeof(MappingProfile));
 
-
-builder.Services.AddAuthorization(
-    policy =>
-    {
-      policy.AddPolicy("AdminOrOwner", policy => policy.Requirements.Add(new AdminOrOwnerRequirement()));
-     }
-);
+builder.Services.AddAuthorization(policy =>
+{
+    policy.AddPolicy(
+        "AdminOrOwner",
+        policy => policy.Requirements.Add(new AdminOrOwnerRequirement())
+    );
+});
 
 var app = builder.Build();
 
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty; // "/swagger/index.html"
+});
 
+
+app.UseCors(options => options.AllowAnyOrigin());
+app.UseHttpsRedirection();
+app.UseMiddleware<ExceptionHandlerMiddleware>();
 app.UseRouting();
-
 app.UseAuthentication(); // extract auth header and validate it
 app.UseAuthorization();
-app.UseMiddleware<ExceptionHandlerMiddleware>();
 
-app.MapControllers();
 
 app.Run();
